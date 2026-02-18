6eb9c90b963a5bcd6ba2e5d4ddaf7c9658b89afd
ExploitDev9989 <miguelquiroz9989@gmail.com>
2026-02-18 15:23:32 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Stack_Push_Pop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack_Push_Pop.Stack stack = new Stack_Push_Pop.Stack(5);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+            Console.WriteLine("Top item: " + stack.Peek());
+            Console.WriteLine("Popped item: " + stack.Pop());
+            Console.WriteLine("Popped item: " + stack.Pop());
+            Console.WriteLine("Popped item: " + stack.Pop());
+            
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Push_Pop
+{
+    internal class Stack
+    {
+        int[] items;
+        int top;
+        int capacity;
+
+        public Stack(int size)
+        {
+            items = new int[size];
+            capacity = size;
+            top = -1;
+        }
+
+        public void Push(int item)
+        {
+            //checking if stack is empty 
+            
+            if (top == capacity - 1)
+            {
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
+            items[++top] = item;
+        }
+
+        public int Pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack Underflow");
+                return -1;
+            }
+            return items[top--];
+        }
+
+        public int Peek()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Stack Is Empty");
+                return -1;
+            }
+            return items[top];
+        }
+    }
+}
